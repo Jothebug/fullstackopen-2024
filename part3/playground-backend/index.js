7f9edd3fe82ac9ce26c78320d413af1406b39745
YenHa <hayenbg2603@gmail.com>
2024-05-21 09:41:31 +0700

    playground backend part 3

--- part3/playground-backend/index.js
+++ part3/playground-backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -68,6 +70,21 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(requestLogger);
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {

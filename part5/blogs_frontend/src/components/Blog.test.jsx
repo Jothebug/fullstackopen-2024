17cd6114677ff4b7d07b96706369658b8ba7d3fe
YenHa <hayenbg2603@gmail.com>
2024-06-06 01:08:34 +0700

    solve ex:5.13

--- part5/blogs_frontend/src/components/Blog.test.jsx
+++ part5/blogs_frontend/src/components/Blog.test.jsx
@@ -1,25 +1,33 @@
 /* eslint-disable no-undef */
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders content", () => {
   const blog = {
     title: "test title",
-    url: "",
     author: "Jo",
+    url: "http://test",
+    like: 1,
   };
 
-  render(<Blog item={blog} />);
+  const { container } = render(<Blog item={blog} />);
 
-  const hasTitle = screen.getByText("test title");
-  expect(hasTitle).toBeDefined();
-
-  const hasAuthor = screen.getByText("Jo");
-  expect(hasAuthor).toBeDefined();
-
-  const hasNoUrl = screen.getByText("");
-  expect(hasAuthor).toBeDefined();
-
-  const hasNoLike = screen.getByText(0);
-  expect(hasNoLike).toBeNotDefined();
+  const div = container.querySelector(".blog");
+  expect(div).toHaveTextContent("test title");
+  expect(div).toHaveTextContent("Jo");
+  expect(div).not.toHaveProperty("url");
+  expect(div).not.toHaveProperty("like");
 });
+
+// test("click the View button", () => {
+//   const blog = {
+//     title: "test title",
+//     author: "Jo",
+//     url: "http://test",
+//     like: 1,
+//   };
+
+//   const mockHandler = vi.fn();
+//   render(<Blog item={blog} />);
+// });

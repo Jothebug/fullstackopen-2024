d5f44c4cbeede9b60bfccf057866b67d810ea813
YenHa <hayenbg2603@gmail.com>
2024-06-05 23:59:07 +0700

    fix ex:5.13

--- part5/blogs_frontend/src/components/Blog.test.jsx
+++ part5/blogs_frontend/src/components/Blog.test.jsx
@@ -5,13 +5,21 @@ import Blog from "./Blog";
 test("renders content", () => {
   const blog = {
     title: "test title",
-    url: "http:// test",
-    likes: 1,
+    url: "",
     author: "Jo",
   };
 
   render(<Blog item={blog} />);
 
-  const element = screen.getByText("test title");
-  expect(element).toBeDefined();
+  const hasTitle = screen.getByText("test title");
+  expect(hasTitle).toBeDefined();
+
+  const hasAuthor = screen.getByText("Jo");
+  expect(hasAuthor).toBeDefined();
+
+  const hasNoUrl = screen.getByText("");
+  expect(hasAuthor).toBeDefined();
+
+  const hasNoLike = screen.getByText(0);
+  expect(hasNoLike).toBeNotDefined();
 });

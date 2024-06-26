d032b1db74ab76698b87ebd50a3f4a0b6228a678
YenHa <hayenbg2603@gmail.com>
2024-06-26 11:46:28 +0700

    solve ex:6.1-6.2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,23 +1,25 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+    case "GOOD":
+      let goodCounter = state.good;
+      return { ...state, good: ++goodCounter };
+    case "OK":
+      let okCounter = state.ok;
+      return { ...state, ok: ++okCounter };
+    case "BAD":
+      let badCounter = state.bad;
+      return { ...state, bad: ++badCounter };
+    case "ZERO":
+      return initialState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;

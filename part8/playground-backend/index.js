33df359db4c5ed2122cbecc4ec9d5c3e0b6de81a
YenHa <hayenbg2603@gmail.com>
2024-07-16 16:04:14 +0700

    playground-backend part8

--- part8/playground-backend/index.js
+++ part8/playground-backend/index.js
@@ -1,18 +1,17 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { GraphQLError } = require("graphql");
-const { v1: uuid } = require("uuid");
-
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 require("dotenv").config();
 
 const Person = require("./models/person");
+const User = require("./models/user");
+const typeDefs = require("./typeDefs");
 
 const MONGODB_URI = process.env.MONGODB_URI;
-
 console.log("connecting to", MONGODB_URI);
-
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
@@ -22,45 +21,6 @@ mongoose
     console.log("error connection to MongoDB:", error.message);
   });
 
-const typeDefs = `
-  type Address {
-     street: String!
-     city: String!
-  }
-
-  type Person {
-    name: String!
-    phone: String
-    address: Address!
-    id: ID!
-  }
-
-  enum YesNo {
-     YES
-     NO
-  }
-
-  type Query {
-    personCount: Int!
-    allPersons(phone: YesNo): [Person!]!
-    findPerson(name: String!): Person
-  }
-
-  type Mutation {
-    addPerson(
-     name: String!
-     phone: String
-     street: String!
-     city: String!
-    ): Person
-    
-    editNumber(
-     name: String!
-     phone: String!
-    ): Person
-  }
-`;
-
 const resolvers = {
   Query: {
     personCount: () => Person.collection.countDocuments(),
@@ -69,6 +29,7 @@ const resolvers = {
       return Person.find({ phone: { $exists: args.phone === "YES" } });
     },
     findPerson: async (_, args) => Person.findOne({ name: args.name }),
+    me: (_, args, context) => context.currentUser,
   },
   Person: {
     address: ({ street, city }) => {
@@ -76,10 +37,19 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: async (_, args) => {
+    addPerson: async (_, args, context) => {
       const person = new Person({ ...args });
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+
       try {
         await person.save();
+        currentUser.friends = currentUser.friends.concat(person);
+        await currentUser.save();
       } catch (error) {
         throw new GraphQLError("Name must be unique", {
           extensions: { code: "BAD_USER_INPUT", invalidArgs: args.name, error },
@@ -103,12 +73,49 @@ const resolvers = {
       }
       return person;
     },
+    createUser: async (_, args) => {
+      const user = new User({ username: args.username });
+      return user.save().catch((error) => {
+        throw new GraphQLError("Creating the user failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.username,
+            error,
+          },
+        });
+      });
+    },
+    login: async (_, args) => {
+      const user = await User.findOne({ username: args.username });
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("Wrong credentials", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
+      const userForToken = { username: user.username, id: user._id };
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 startStandaloneServer(server, {
   listen: { port: process.env.PORT },
+  context: async ({ req, _ }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id).populate(
+        "friends"
+      );
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });

ef5d4100fbb1af17977010458f3b810c103fa936
YenHa <hayenbg2603@gmail.com>
2024-08-06 17:28:08 +0700

    ex:9.1-9.3

--- part9/initTs/bmiCalculator.ts
+++ part9/initTs/bmiCalculator.ts
@@ -1,3 +1,8 @@
+interface Values {
+  value1: number;
+  value2: number;
+}
+
 const bmiCategory = (bmi: number): string => {
   if (bmi < 16.0) {
     return `Underweight (Severe thinness) - ${bmi}`;
@@ -18,6 +23,20 @@ const bmiCategory = (bmi: number): string => {
   }
 };
 
+const parseArguments = (args: string[]): Values => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   if (typeof height !== "number" || typeof weight !== "number")
     throw new Error("values must be number");
@@ -28,4 +47,13 @@ const calculateBmi = (height: number, weight: number): string => {
   return bmiCategory(BMI);
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
